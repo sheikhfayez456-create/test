2758b7730c944d4d1db72737986a45b65d33989a
sheikhfayez456-create <sheikhfayez456@gmail.com>
2025-10-03 17:52:11 +0500

    Update add.py

--- add.py
+++ add.py
@@ -1,1 +1,26 @@
 print("hi")
+
+print("hi")
+
+# A very basic Python program
+
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Ask user for two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform calculations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Show results
+print("\nHello,", name)
+print("Here are the results of your numbers:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
